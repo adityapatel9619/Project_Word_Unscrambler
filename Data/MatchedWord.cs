0ab0e191c83f39d400c07747e897f222343775fa
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-07-28 23:11:30 +0530

    2

---
+++ Data/MatchedWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler.Data
+{
+    public struct MatchedWord
+    {
+        public string ScrambleWords { get; set; }
+        public string Word { get; set; }
+    }
+}
